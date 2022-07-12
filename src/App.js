b34208419e952f267df70f7edf5b3033683e0f73
Khamzaatov <adamkhamzatov9522@gmail.com>
2022-07-12 12:49:46 +0300

    complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './style.css'
 
 function App() {
+  const [text, setText] = useState()
+  const [empty, setEmpty] = useState(true)
+  const [send, setSend] = useState(false)
+  const [blur, setBlur] = useState(false)
+
+  const handleText = (e) => {
+    setText(e.target.value)
+    if (!e.target.value) {
+      setEmpty(true)
+    } else {
+    setEmpty(false)
+    setSend(false)
+    }
+  }
+  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+
+    console.log(text)
+    setText('')
+    setEmpty(true)
+    setSend(true)
+    setBlur(false)
+  }
+
+  const hadnleBlur = () => {
+    setBlur(true)
+    setSend(false)
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={text} onChange={handleText} onBlur={hadnleBlur} />
+        <button type="submit" disabled={empty}>Клик</button>
+      </form>
+      {(empty && blur) && <i><div className='error'>Поле ввода не может быть пустым!</div></i>}
+      {send ? <i><div className='send'>Сообщение отправлено!</div></i> : null}
     </div>
   );
 }
